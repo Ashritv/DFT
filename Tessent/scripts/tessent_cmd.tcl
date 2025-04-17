1c4e7b0abf33c9804c30bfe838152a62bf07f2f9
Ashritv <ashritamblima@gmail.com>
2025-04-17 17:42:26 +0530

    tessent cmds log

--- Tessent/scripts/tessent_cmd.tcl
+++ Tessent/scripts/tessent_cmd.tcl
@@ -2,10 +2,11 @@ set_context dft -scan
 read_verilog opt.v
 read_cell_library test_Nangate.mdt 
 set_current_design mcrb
+set_design_level top
 analyze_control_signals -auto
 check_design_rules
 analyze_scan_chains 
-insert_test_logic
+insert_test_logic -write_in_tsdb on 
 
 report_scan_elements > ./output/mcrb_scan_elements.txt
 report_scan_chains > ./output/mcrb_scan_chains.txt
@@ -16,5 +17,5 @@ write_design -output_file ./output/mcrb_tessent_scan_net.v -replace
 set_system_mode setup
 open_visualizer
 
-insert_test_logic -write_in_tsdb on 
+
 
