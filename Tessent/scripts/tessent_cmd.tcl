f29b49d0e1b643b8e88018e7154e30335ab7ad58
Ashritv <ashritamblima@gmail.com>
2025-04-21 10:08:50 +0530

    Updated cmds

--- Tessent/scripts/tessent_cmd.tcl
+++ Tessent/scripts/tessent_cmd.tcl
@@ -4,7 +4,11 @@ read_cell_library test_Nangate.mdt
 set_current_design mcrb
 set_design_level top
 analyze_control_signals -auto
-check_design_rules
+
+#Run DRC
+check_design_rules # Or can Use set_system_mode analysis does the same thing
+
+set_scan_insertion_options -chain_count 5
 analyze_scan_chains 
 insert_test_logic -write_in_tsdb on 
 
@@ -17,5 +21,6 @@ write_design -output_file ./output/mcrb_tessent_scan_net.v -replace
 set_system_mode setup
 open_visualizer
 
-
-
+#The insert_test_logic -write_in_tsdb on command creates the following:
+    # 1.The scan stitched and inserted design.
+    # 2.TCD file containing the Scan Configurations to be used during ATPG
